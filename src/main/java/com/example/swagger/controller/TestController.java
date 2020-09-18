cf6fc51a84608344612fb4c0dedfc4ae6b968152
g-hyoga <hyoga0216@gmail.com>
2020-09-18 12:09:22 +0900

    Introduce swagger

--- src/main/java/com/example/swagger/controller/TestController.java
+++ src/main/java/com/example/swagger/controller/TestController.java
@@ -1,14 +1,24 @@
 package com.example.swagger.controller;
 
+import java.util.Optional;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import reactor.core.publisher.Mono;
 
 @RestController
 public class TestController {
+
+
+    @ApiResponses(value = {@ApiResponse(responseCode = "200")})
     @GetMapping(value = "/v1/ping")
-    public Mono<String> getDeauContents() {
+    public Mono<String> getDeauContents(
+            @RequestParam(name = "message", required = true) Optional<String> message
+    ) {
         return Mono.just("pong");
     }
 }
